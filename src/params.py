988c842ed9d321a207eba328d0d80b32d44825e8
Rayan DAOD <rayan.daodnathoo@epfl.ch>
2019-11-08 15:44:08 +0100

    Restructure and add useful files

---
+++ src/params.py
@@ -0,0 +1,8 @@
+
+# Path variables
+DATA_TRAIN_POS_PATH = "data/train_pos.txt"
+DATA_TRAIN_NEG_PATH = "data/train_neg.txt"
+DATA_TRAIN_POS_FULL_PATH = "data/train_pos_full.txt"
+DATA_TRAIN_NEG_FULL_PATH = "data/train_neg_full.txt"
+DATA_TEST_PATH = "data/test_data.txt"
+
