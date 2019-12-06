6535c12c2005c4c6328d17fe5966fdf44f224fbf
Kopiga Rasiah <kopiga.rasiah@epfl.ch>
2019-12-06 11:15:35 +0100

    implement NN

--- src/glove_solution.py
+++ src/glove_solution.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import os
 import sys
-from numba import jit, cuda
+#from numba import jit, cuda
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_PATH)
@@ -13,7 +13,7 @@ import src.paths as paths
 import src.params as params
 
 
-@jit(target="cuda")
+#@jit(target="cuda")
 def main():
     print("loading cooccurrence matrix")
     with open(paths.COOC_PICKLE, 'rb') as f:
@@ -24,7 +24,7 @@ def main():
     print("using nmax =", nmax, ", cooc.max() =", cooc.max())
 
     print("initializing embeddings")
-    embedding_dim = paths.K
+    embedding_dim = params.K
     xs = np.random.normal(size=(cooc.shape[0], embedding_dim))
     ys = np.random.normal(size=(cooc.shape[1], embedding_dim))
 
